cef586ebead39a12cb97a51eb50382eae70b213b
Robin Treur <robin@0-to-9.nl>
2022-01-24 14:57:06 +0100

    Changed version to 2.0.4

--- system/user/addons/seo_lite/mcp.seo_lite.php
+++ system/user/addons/seo_lite/mcp.seo_lite.php
@@ -51,7 +51,7 @@ class Seo_lite_mcp
         
         ee('CP/URL', 'addons/settings/seo_lite/audit_entry');
 
-        ee()->cp->add_to_head("<link rel='stylesheet' href='" . URL_THIRD_THEMES . "seo_lite/css/seo_lite.css?v2.0.3'>");
+        ee()->cp->add_to_head("<link rel='stylesheet' href='" . URL_THIRD_THEMES . "seo_lite/css/seo_lite.css?v2.0.4'>");
 	}
 
 	function index() 
--- system/user/addons/seo_lite/upd.seo_lite.php
+++ system/user/addons/seo_lite/upd.seo_lite.php
@@ -13,7 +13,7 @@
 class Seo_lite_upd
 {
 
-    var $version = '2.0.3';
+    var $version = '2.0.4';
     var $module_name = "Seo_lite";
 
     /**
